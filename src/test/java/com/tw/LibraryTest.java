e01d76b93a6f7a5df26404ba9742b49ad3605f30
king <369558172@qq.com>
2018-04-11 22:57:39 +0800

    ADD TEST METHOD

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,7 +1,11 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import static org.hamcrest.core.Is.is;
@@ -10,7 +14,41 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
+    ByteArrayOutputStream outContent;
+
+    @Before
+    public void setUp() throws Exception {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
+    public void testjudgeStudentInfo () {
+        String s1 = "张三,123,数学:75,语文:95,英语:80,编程:80";
+        assertTrue(Library.addStudentInfo(Arrays.asList(s1.split(","))));
+
+        String s2 = "abc,123,数学:75,语文:95,英语:80,编程:80";
+        Library.addStudentInfo(Arrays.asList(s2.split(",")));
+        assertTrue(Library.addStudentInfo(Arrays.asList(s2.split(","))));
+
+        String s3 = "张三,123,数学:75,语文:95,英语:80";
+        Library.addStudentInfo(Arrays.asList(s3.split(",")));
+        assertTrue(!Library.addStudentInfo(Arrays.asList(s3.split(","))));
+
+        String s4 = "张三,学号,数学:75,语文:95,英语:80,编程:80";
+        Library.addStudentInfo(Arrays.asList(s4.split(",")));
+        assertTrue(!Library.addStudentInfo(Arrays.asList(s4.split(","))));
+
+        String s5 = "张三,123,数学:75,语文:95,英语:130,编程:80";
+        Library.addStudentInfo(Arrays.asList(s5.split(",")));
+        assertTrue(!Library.addStudentInfo(Arrays.asList(s5.split(","))));
+
+    }
+/*    @Test
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
@@ -27,6 +65,6 @@ public class LibraryTest {
 
         assertEquals(mockedList.get(0), value);
 
-    }
+    }*/
 
 }
